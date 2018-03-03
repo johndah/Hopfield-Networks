4ea0b0877a10e164d565ebbffbb3f4a56ea9c962
John Dahlberg <john.dahlberg@live.com>
2018-03-03 16:03:12 +0100

    Restoring distorted inputs

--- HopfieldNetworks.py
+++ HopfieldNetworks.py
@@ -11,32 +11,44 @@ class HopfieldNetwork(object):
 
     def hopFieldNetworkAlgorithm(self):
         self.hebbianLearning(self.X)
-        X = self.recall(self.X)
-        print(X-self.X)
+
 
     def hebbianLearning(self, X):
+        #for
+        deltaW = dot(X.T, X)
+        self.W += deltaW
+
         # for i in range(X.shape[1]):
         #    x = array([X[:, i]])
         #    print(x)
         #    self.W += dot(x.T, x)
 
-        self.W += dot(X.T, X)
-        #print(self.W)
-
     def recall(self, X):
         X = sign(dot(self.W, X.T))
 
         return X.T
-
-
-def main():
-
+def generateData():
     x1 = array([[-1, -1, 1, -1, 1, -1, -1, 1]])
     x2 = array([[-1, -1, -1, -1, -1, 1, -1, -1]])
     x3 = array([[-1, 1, 1, -1, -1, 1, -1, 1]])
     X = concatenate([x1, x2, x3], axis=0)
+
+    x1 = array([[1, -1, 1, -1, 1, -1, -1, 1]])
+    x2 = array([[1, -1, -1, -1, -1, 1, -1, -1]])
+    x3 = array([[1, 1, 1, -1, 1, 1, -1, 1]])
+    Xd = concatenate([x1, x2, x3], axis=0)
+
+    return X, Xd
+
+def main():
+
+    X, Xd = generateData()
     hn = HopfieldNetwork(X)
     hn.hopFieldNetworkAlgorithm()
+    Xrecalled = hn.recall(X)
+    print(Xrecalled - X)
+    XDrecalled = hn.recall(Xd)
+    print(XDrecalled - X)
 
 if __name__ == '__main__':
     main()
